fcd86544ac0e40f114a9a9aab3216de4295d72b8
lucaconigliaro <lucaconigliaro1@gmail.com>
2024-12-20 12:57:00 +0100

    Aggiunti nuovi campi, utilizzato un unico oggetto per gestire il form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,166 @@
+import { useState } from "react";
 
+// Struttura base per un nuovo articolo
+const initialFormData = {
+  id: "",
+  title: "",
+  author: "",
+  image: "",
+  content: "",
+  category: "",
+};
 
 function App() {
 
+  const [article, setArticle] = useState([]);
+
+  const [formData, setFormData] = useState(initialFormData);
+
+  // Gestione del modulo quando viene inviato
+  const handleArticleForm = (event) => {
+    event.preventDefault();
+    if (formData.title !== "" && formData.author !== "") {
+
+      const newArticle = {
+        ...formData, // Copia tutti i dati attualmente nel modulo.
+        id: Date.now() // Aggiunge un ID unico basato sull'ora attuale.
+      };
+
+      const newArray = [...article, newArticle]; // Crea un nuovo array con l'articolo aggiunto.
+      setArticle(newArray);
+
+      setFormData(initialFormData);
+    };
+  };
+
+  const cancel = (idToDelete) => {
+    const newArray = article.filter(curArticle => curArticle.id !== idToDelete);
+    setArticle(newArray);
+  };
+
+  const handleInputChange = (event) => {
+    const keyToChange = event.target.name; // Nome del campo modificato.
+    const newValue = event.target.value; // Nuovo valore inserito.
+
+    const newData = {
+      ...formData,
+      [keyToChange]: newValue
+    };
+
+    setFormData(newData);
+  };
+
+
   return (
     <>
-   
+      <div className="container">
+        <section>
+          <h2>Nuovi Articoli</h2>
+          {article.length > 0 ? (
+            <div className="row row-cols-2">
+              {article.map((curArticle) => (
+                <div className="col" key={curArticle.id}>
+                  <div className="card">
+                    <img src={curArticle.image} alt={curArticle.title} className="card-img-top" />
+                    <div className="card-body">
+                      <h4>{curArticle.title}</h4>
+                      <p>Autore: {curArticle.author}</p>
+                      <p>Categoria: {curArticle.category}</p>
+                      <p>{curArticle.content}</p>
+                      <button onClick={() => cancel(curArticle.id)} className="btn btn-danger">🗑️ Elimina</button>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <p>Nessun articolo</p>
+          )}
+        </section>
+
+        <section>
+          <h3>Aggiungi un nuovo articolo</h3>
+          <form onSubmit={handleArticleForm}>
+            <div>
+              <label htmlFor="articleTitle">Nome dell'articolo</label>
+              <input
+                id="articleTitle"
+                type="text"
+                className="form-control mb-2"
+                placeholder="Inserisci il titolo dell'articolo"
+                name="title"
+                value={formData.title}
+                onChange={handleInputChange}
+              />
+            </div>
+
+            <div>
+              <label htmlFor="articleAuthor">Autore</label>
+              <input
+                id="articleAuthor"
+                type="text"
+                className="form-control mb-2"
+                placeholder="Inserisci l'Autore"
+                name="author"
+                value={formData.author}
+                onChange={handleInputChange} />
+            </div>
+
+            <div>
+              <label htmlFor="articleImage">Immagine dell'articolo</label>
+              <input
+                id="articleImage"
+                type="text"
+                className="form-control mb-2"
+                placeholder="Inserisci l'immagine dell'articolo"
+                name="image"
+                value={formData.image}
+                onChange={handleInputChange}
+              />
+            </div>
+
+            <div>
+              <label htmlFor="articleContent">Contenuto dell'articolo</label>
+              <textarea
+                id="articleContent"
+                className="form-control mb-2"
+                placeholder="Inserisci il contenuto dell'articolo"
+                name="content"
+                value={formData.content}
+                onChange={handleInputChange}
+              ></textarea>
+            </div>
+
+            <div>
+              <label htmlFor="articleCategory">Categoria dell'articolo</label>
+              <select
+                id="articleCategory"
+                className="form-control mb-2"
+                name="category"
+                value={formData.category}
+                onChange={handleInputChange}
+              >
+                <option value="" disabled>Seleziona una categoria</option>
+                <option value="tecnologia">Tecnologia</option>
+                <option value="scienza">Scienza</option>
+                <option value="arte">Arte</option>
+                <option value="sport">Sport</option>
+              </select>
+            </div>
+
+            <button
+              type="submit"
+              className="btn btn-success"
+              disabled={!formData.title || !formData.author || !formData.content || !formData.category}
+            >
+              Invia
+            </button>
+
+          </form>
+        </section>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default App
---
+++ src/data/data.js
@@ -0,0 +1,40 @@
+const posts = [
+    {
+      id: 1,
+      title: "Titolo del Post",
+      image: 'https://fastly.picsum.photos/id/384/200/200.jpg?hmac=TAsUnIQnfLj13hjjp8604_rXNHrqCGlB2K8UQoWt1aM',
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+      tags: ["html", "css"],
+      published: true,
+    },
+    {
+      id: 2,
+      title: "Titolo del Post",
+      image: 'https://placehold.co/600x400',
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+      tags: ["js", "css"],
+      published: true,
+    },
+    {
+      id: 3,
+      title: "Titolo del Post",
+      image: 'https://placehold.co/600x400',
+      content:
+        "Lorem ipsum dolor sit amet consecttur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+      tags: ["js", "php"],
+      published: true,
+    },
+    {
+      id: 4,
+      title: "Titolo del Post",
+      image: 'https://fastly.picsum.photos/id/260/200/200.jpg?hmac=Nu9V4Ixqq3HiFhfkcsL5mNRZAZyEHG2jotmiiMRdxGA',
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+      tags: ["html"],
+      published: false,
+    },
+  ];
+
+  export default posts
